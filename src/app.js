d85a678ea2efe0a87eef1bf38b9a6db42dc294bb
pranavsonawane5 <pranavrsonawane04@gmail.com>
2025-01-22 12:05:49 +0530

    created controller

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+console.log("Routes registered : /api/v1/users");
+
+//http://localhost:8000/api/v1/users/register
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,14 @@
-import { Promise } from "mongoose"
+import mongoose from "mongoose"
 
 const asyncHandler  = (requestHandler) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
 }
 
 
-
 export {asyncHandler}
+
 
 
 
